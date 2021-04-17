726252f0f608566c298a7fcb395d7ddcf9887a27
Pedro Silva <pdx.henrique@gmail.com>
2021-04-17 20:02:13 -0300

    Replace Fabric Crashlytics with Firebase Crashlytics

---
+++ README.md
@@ -0,0 +1,6 @@
+# TecnoNutri Feed
+
+## Setup
+
+* Register project in Firebase Console for applications id `br.com.pedrosilva.tecnonutri.frio` and `br.com.pedrosilva.tecnonutri.quente`
+* Put `google-services.json` in `app/` directory
--- app/src/main/kotlin/br/com/pedrosilva/tecnonutri/AndroidApplication.kt
+++ app/src/main/kotlin/br/com/pedrosilva/tecnonutri/AndroidApplication.kt
@@ -2,16 +2,13 @@ package br.com.pedrosilva.tecnonutri
 
 import android.app.Application
 import br.com.pedrosilva.tecnonutri.util.ContextHelper
-import com.crashlytics.android.Crashlytics
 import com.pedrenrique.tecnonutri.data.DataModule
-import io.fabric.sdk.android.Fabric
 
 class AndroidApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         ContextHelper.init(applicationContext)
-        Fabric.with(this, Crashlytics())
         initData()
     }
 
